3a2c2abc2fce531e897d3e68c5643e138831e374
Kym Eden <shikasta.net@gmail.com>
2020-12-02 09:04:19 +0000

    Find snapshots matching target folder

--- sbin/zfsnap-back.sh
+++ sbin/zfsnap-back.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
+target="$1"
 
+zfs_root() {
+  t=$1
+  while [ ! -d "$t/.zfs" ] ; do
+    t="${t%/*}"
+  done
+  echo "$t"
+}
+zfsroot="$(zfs_root $target)"
+subpath="${target#$zfsroot}"
+
+selected=$(ls -d "$zfsroot/.zfs/snapshot/"*"$subpath" | sed "s|$zfsroot/\.zfs/snapshot/\(.*\)$subpath|\1|" | sort -r | fzf)
+
+echo Selected $selected

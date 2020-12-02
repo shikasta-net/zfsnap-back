0744060d24a58bfb79e1b1b4b47190d15f3d76f3
Kym Eden <kym@shodan.tk>
2020-12-02 10:06:21 +0000

    Confirm and copy selection

--- sbin/zfsnap-back.sh
+++ sbin/zfsnap-back.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-target="$1"
+target=$(readlink -f "$1")
+
+snapshot_dir="/.zfs/snapshot"
 
 zfs_root() {
   t=$1
@@ -12,6 +14,13 @@ zfs_root() {
 zfsroot="$(zfs_root $target)"
 subpath="${target#$zfsroot}"
 
-selected=$(ls -d "$zfsroot/.zfs/snapshot/"*"$subpath" | sed "s|$zfsroot/\.zfs/snapshot/\(.*\)$subpath|\1|" | sort -r | fzf)
+selected=$(ls -d "$zfsroot$snapshot_dir/"*"$subpath" | sed "s|$zfsroot$snapshot_dir/\(.*\)$subpath|\1|" | sort -r | fzf)
 
-echo Selected $selected
+read -p "Restore $target from $selected snapshot? [N] " reply
+case "$reply" in
+  y|Y ) ;;
+  * ) echo aborted; [ "$0" = "$BASH_SOURCE" ] && exit 1 || return 1;; # handle exits from shell or function but don't exit interactive shell
+esac
+
+rsync -a "$zfsroot$snapshot_dir/$selected$subpath" "$target"
+

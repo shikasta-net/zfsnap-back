679ec014cfbfe1b51f86631bd4074c263c689a9f
Kym Eden <kym@shodan.tk>
2021-10-23 22:05:10 +0100

    Fix directory restore nesting and clean-up contents if necessary

--- sbin/zfsnap-back.sh
+++ sbin/zfsnap-back.sh
@@ -17,11 +17,17 @@ escaped_subpath=$(echo $subpath | sed "s|\[|\\\[|g" | sed "s|\]|\\\]|g")
 
 selected="$(ls -d "$zfsroot$snapshot_dir/"*"$subpath" | sed "s|$zfsroot$snapshot_dir/\(.*\)$escaped_subpath|\1|" | sort -r | fzf)"
 
+source="$zfsroot$snapshot_dir/$selected$subpath"
+
 read -p "Restore $target from $selected snapshot? [N] " reply
 case "$reply" in
   y|Y ) ;;
   * ) echo aborted; [ "$0" = "$BASH_SOURCE" ] && exit 1 || return 1;; # handle exits from shell or function but don't exit interactive shell
 esac
 
-rsync -a "$zfsroot$snapshot_dir/$selected$subpath" "$target"
+if [ -d "$source" ] ; then
+  target=$(dirname "$target")
+fi
+
+rsync -a --delete "$source" "$target"
 
